c4124dbda63426c97174ae66fddf2540d96874eb
kukujiabo <kukujiabo@163.com>
2018-06-15 18:54:06 +0800

    123

--- src/app/Api/Shop.php
+++ src/app/Api/Shop.php
@@ -17,7 +17,7 @@ class Shop extends BaseApi {
       
         'mid' => 'mid|int|true||商户id',
         'shop_name' => 'shop_name|string|true||店铺名称',
-        'shop_code' => 'shop_code|string|true||店铺编码',
+        'shop_code' => 'shop_code|string|false||店铺编码',
         'phone' => 'phone|string|true||店铺电话',
         'open_time' => 'open_time|string|false||营业时间',
         'thumbnail' => 'thumbnail|string|false||店铺logo',
