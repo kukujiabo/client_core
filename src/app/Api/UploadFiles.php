892b04c169d80deebcd745e4d77200a39a260b63
kukujiabo <kukujiabo@163.com>
2018-06-20 13:46:18 +0800

    123

---
+++ src/app/Api/UploadFiles.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Api;
+
+/**
+ * 上传文件
+ *
+ * @author Meroc Chen
+ */
+class UploadFiles extends BaseApi {
+
+  public function getRules() {
+  
+  
+    return $this->rules([
+    
+      'upload' => [
+      
+        'upfile' => [
+        
+          'name' => 'upfile',
+          'type' => 'file',
+          'min' => 0,
+          'max' => 1024 * 1024 * 1024,
+          'range' => [ 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel' ],
+          'ext' => [ 'xlsx', 'xls' ]
+        
+        ]
+      
+      ]   
+    
+    ]);
+  
+  }
+
+  /**
+   * 上传文件
+   * @desc 上传文件
+   *
+   * @return array data
+   */
+  public function upload() {
+  
+    return $this->dm->upload($this->retriveRuleParams(__FUNCTION__)); 
+  
+  }
+
+}
---
+++ src/app/Domain/UploadFilesDm.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Domain;
+
+class UploadFilesDm {
+
+  public function update($data) {
+  
+    return $data;
+  
+  }
+
+}
