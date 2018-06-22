efc5584c0b27da4273ccd9c8122d75eb1e4c75f2
kukujiabo <kukujiabo@163.com>
2018-06-23 02:19:03 +0800

    123

---
+++ src/app/Model/VBapplyMemberLoan.php
@@ -0,0 +1,7 @@
+<?php
+namespace App\Model;
+
+class VBapplyMemberLoan extends BaseModel {
+
+
+}
---
+++ src/app/Service/Crm/VBapplyMemberLoanSv.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Service\Crm;
+
+use App\Service\BaseService;
+use Core\Service\CurdSv;
+
+/**
+ * 信用卡申请视图
+ *
+ */
+class VBapplyMemberLoanSv extends BaseService {
+
+  use CurdSv;
+
+}
