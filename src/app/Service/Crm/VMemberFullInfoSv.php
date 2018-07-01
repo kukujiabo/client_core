5aa6380017b395e873ae960cb2de7042346f422e
kukujiabo <kukujiabo@163.com>
2018-07-01 11:41:01 +0800

    新增会员内联业务信息模型

---
+++ src/app/Service/Crm/VMemberFullInfoSv.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Service\Crm;
+
+use Core\Service\CurdSv;
+use App\Service\BaseService;
+
+/**
+ * 会员內联合信息
+ *
+ */
+class VMemberFullInfoSv extends BaseService {
+
+  use CurdSv;
+
+}
