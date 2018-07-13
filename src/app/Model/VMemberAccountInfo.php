1da62df2a4ec09aead4d6d3762e99435980dfb39
kukujiabo <kukujiabo@163.com>
2018-07-13 22:56:54 +0800

    123 /

--- src/app/Model/VMemberAccountInfo.php
+++ src/app/Model/VMemberAccountInfo.php
@@ -3,5 +3,12 @@ namespace App\Model;
 
 class VMemberAccountInfo extends BaseModel {
 
+  protected $_queryOptionRule = [
+  
+    'member_name' => 'like',
+    'alipay_account' => 'like',
+    'alipay_realname' => 'like'
+  
+  ];
 
 }
